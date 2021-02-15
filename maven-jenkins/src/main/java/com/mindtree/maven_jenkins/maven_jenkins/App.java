9e810a69bd17e17f0452aa0dfc6589c5b6483bf4
liki2124 <likithasgowdablr@gmail.com>
2021-02-15 12:54:32 +0530

    first commit

---
+++ maven-jenkins/src/main/java/com/mindtree/maven_jenkins/maven_jenkins/App.java
@@ -0,0 +1,13 @@
+package com.mindtree.maven_jenkins.maven_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{ //hello
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
